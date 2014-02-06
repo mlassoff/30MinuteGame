07f559e83dad81f1739e77000c7e1d397a21ee23
Mark Lassoff <mark@learntoprogram.tv>
2014-02-06 15:51:05 -0500

    Added crosshair, mouse control, sound
    Started sprite animation— incomplete

--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 var context;
 var queue;
-var WIDTH = 300;
-var HEIGHT = 500;
+var WIDTH = 1024;
+var HEIGHT = 768;
+var mouseXPosition;
+var mouseYPosition;
+var batImage;
+var stage;
 
 window.onload = function()
 {
@@ -13,19 +17,79 @@ window.onload = function()
     context = canvas.getContext('2d');
     context.canvas.width = WIDTH;
     context.canvas.height = HEIGHT;
-
+    stage = new createjs.Stage("myCanvas");
     /*
-     *      Set up the Asset Queue
+     *      Set up other events and delegats
+     *
+     */
+    window.onmousemove = handleMouseMove;
+    window.onmousedown = handleMouseDown;
+    
+    
+    /*
+     *      Set up the Asset Queue and load sounds
      *
      */
     queue = new createjs.LoadQueue(false);
     queue.on("complete", queueLoaded, this);
+    createjs.Sound.alternateExtensions = ["ogg"];
     queue.loadFile({id:"backgroundImage", src:"assets/background.png"});
+    queue.loadFile({id:"crossHair", src:"assets/crosshair.png"});
+    queue.loadFile({id:"shot", src:"assets/shot.mp3"});
+    queue.loadFile({id:"background", src:"assets/countryside.mp3"});                
     queue.load();
+    
+    /*
+     *      Initialize Bat Sprite Sheet
+     *
+     */
+    batImage = new Image();
+    batImage.onload = handleBatLoad;
+    batImage.onError = handleBatError;
+    batImage.src = "assets/batSpritesheet.png";
 }
+
+function handleBatLoad()
+{
+    var spriteSheet = new createjs.SpriteSheet({
+            images: [batImage],
+            frame: {width: 64, height: 64, regX: 32, regY: 32},
+            animations: {
+                    flap: [0,9, "flap"]
+            }
+    });
+    
+    bmpAnimation = new creatjs.BitmapAnimation(spriteSheet);
+    
+    bmpAnimation.gotoAndPlay("flap");
+    
+    bmpAnimation.name="bat1";
+    bmpAnimation.direction = 90;
+    bmpAnimation.vx =4;
+    bmpAnimation.x = 16;
+    bmpAnimation.y = 32;
+    bmpAnimation.currentFrame = 0;
+    stage.addChild(bmpAnimation);
+}
+
 
 function queueLoaded(event)
 {
+    createjs.Sound.registerSound("assets/shot.mp3", "shot");
+    createjs.Sound.registerSound("assets/countryside.mp3", "background");
     context.drawImage(queue.getResult("backgroundImage"),0,0);    
+    var backgroundSound = createjs.Sound.play("background");
     
+}
+
+function handleMouseMove(event)
+{
+    context.drawImage(queue.getResult("backgroundImage"),0,0);    
+    context.drawImage(queue.getResult("crossHair"), event.clientX-45, event.clientY-45);   
+}
+
+function handleMouseDown(event)
+{
+    var instance = createjs.Sound.play("shot");
+   
 }
